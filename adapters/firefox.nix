99664add631c09d664e2f91a134cb56c8b078be8
Vojtěch Fošnár <me@vfosnar.cz>
2024-05-11 20:32:05 +0200

    fix toolbar hover?

--- adapters/firefox.nix
+++ adapters/firefox.nix
@@ -63,22 +63,22 @@
           --lwt-selected-tab-background-color: #${base01} !important;
           /* toolbar area */
           --toolbarbutton-icon-fill: #${base04} !important;
-          --toolbarbutton-hover-background: #${base01} !important;
-          --lwt-toolbarbutton-hover-background: #${base01} !important;
-          --toolbarbutton-active-background: #${base01} !important;
-          --lwt-toolbarbutton-active-background: #${base01} !important;
+          --toolbarbutton-hover-background: #${base02} !important;
+          --lwt-toolbarbutton-hover-background: #${base02} !important;
+          --toolbarbutton-active-background: #${base02} !important;
+          --lwt-toolbarbutton-active-background: #${base02} !important;
           /* urlbar */
           --toolbar-field-border-color: transparent !important;
           --toolbar-field-focus-border-color: #${base09} !important;
           --urlbar-popup-url-color: #${base08} !important;
           /* urlbar Firefox < 92 */
           --lwt-toolbar-field-background-color: #${base00} !important;
-          --lwt-toolbar-field-focus: #${base01} !important;
+          --lwt-toolbar-field-focus: #${base02} !important;
           --lwt-toolbar-field-color: #${base04} !important;
           --lwt-toolbar-field-focus-color: #${base04} !important;
           /* urlbar Firefox 92+ */
           --toolbar-field-background-color: #${base00} !important;
-          --toolbar-field-focus-background-color: #${base01} !important;
+          --toolbar-field-focus-background-color: #${base02} !important;
           --toolbar-field-color: #${base04} !important;
           --toolbar-field-focus-color: #${base04} !important;
           /* sidebar - note the sidebar-box rule for the header-area */
