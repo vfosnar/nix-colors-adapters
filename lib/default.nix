3bd11b0d8d74b69a0b9d24a414f55c988f60ccef
Vojtěch Fošnár <me@vfosnar.cz>
2024-05-12 12:28:29 +0200

    final fix?

--- lib/default.nix
+++ lib/default.nix
@@ -1,19 +1,19 @@
 {
-  getAccentForTheme = colorScheme: colorScheme.colors.base0D;
-  # let
-  #   dictionary = with colorScheme.colors; {
-  #     "ayu-light" = base0A;
-  #     "ayu-dark" = base0A;
-  #     "gruvbox-dark-hard" = base08;
-  #     "gruvbox-dark-medium" = base08;
-  #     "gruvbox-dark-pale" = base08;
-  #     "gruvbox-dark-soft" = base08;
-  #     "gruvbox-light-hard" = base08;
-  #     "gruvbox-light-medium" = base08;
-  #     "gruvbox-light-soft" = base08;
-  #   };
-  # in
-  # if builtins.hasAttr colorScheme.slug dictionary
-  # then (builtins.getAttr colorScheme.slug dictionary)
-  # else colorScheme.colors.base0D;
+  getAccentForTheme = colorScheme:
+    let
+      dictionary = with colorScheme.colors; {
+        "ayu-light" = base0A;
+        "ayu-dark" = base0A;
+        "gruvbox-dark-hard" = base08;
+        "gruvbox-dark-medium" = base08;
+        "gruvbox-dark-pale" = base08;
+        "gruvbox-dark-soft" = base08;
+        "gruvbox-light-hard" = base08;
+        "gruvbox-light-medium" = base08;
+        "gruvbox-light-soft" = base08;
+      };
+    in
+    if builtins.hasAttr colorScheme.slug dictionary
+    then (builtins.getAttr colorScheme.slug dictionary)
+    else colorScheme.colors.base0D;
 }
