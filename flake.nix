9983e20bfa7a45b021e98d98847c8a588777ab3e
Vojtěch Fošnár <me@vfosnar.cz>
2024-05-12 11:44:43 +0200

    test

--- flake.nix
+++ flake.nix
@@ -4,8 +4,7 @@
   inputs = { nix-colors.url = "github:misterio77/nix-colors"; };
 
   outputs = { self, nix-colors }:
-    let nixColorsAdaptersLib = import ./lib; in {
+    {
       homeManagerModules.default = import ./adapters;
-      lib = nixColorsAdaptersLib;
     };
 }
