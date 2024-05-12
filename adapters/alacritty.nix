5a7cb085aaea3df8fb616151d0539b1ef3a18ecc
Vojtěch Fošnár <me@vfosnar.cz>
2024-05-12 14:53:48 +0200

    convert alacritty and dunst config to nix

--- adapters/alacritty.nix
+++ adapters/alacritty.nix
@@ -6,62 +6,45 @@
   };
 
   config = lib.mkIf config.nixColorsAdapters.alacritty.enable {
-    programs.alacritty.settings = with config.colorScheme.colors; ''
-      # Default colors
-      [colors.primary]
-      background = '0x${base00}'
-      foreground = '0x${base05}'
-
-      # Colors the cursor will use if `custom_cursor_colors` is true
-      [colors.cursor]
-      text = '0x${base00}'
-      cursor = '0x${base05}'
-
-      # Normal colors
-      [colors.normal]
-      black = '0x${base00}'
-      red = '0x${base08}'
-      green = '0x${base0B}'
-      yellow = '0x${base0A}'
-      blue = '0x${base0D}'
-      magenta = '0x${base0E}'
-      cyan = '0x${base0C}'
-      white = '0x${base05}'
-
-      # Bright colors
-      [colors.bright]
-      black = '0x${base03}'
-      red = '0x${base08}'
-      green = '0x${base0B}'
-      yellow = '0x${base0A}'
-      blue = '0x${base0D}'
-      magenta = '0x${base0E}'
-      cyan = '0x${base0C}'
-      white = '0x${base07}'
-
-      [[colors.indexed_colors]]
-      index = 16
-      color = "0x${base09}"
-
-      [[colors.indexed_colors]]
-      index = 17
-      color = "0x${base0F}"
-
-      [[colors.indexed_colors]]
-      index = 18
-      color = "0x${base01}"
-
-      [[colors.indexed_colors]]
-      index = 19
-      color = "0x${base02}"
-
-      [[colors.indexed_colors]]
-      index = 20
-      color = "0x${base04}"
-
-      [[colors.indexed_colors]]
-      index = 21
-      color = "0x${base06}"
-    '';
+    programs.alacritty.settings = with config.colorScheme.colors; {
+      colors = {
+        primary = {
+          background = "0x${base00}";
+          foreground = "0x${base05}";
+        };
+        cursor = {
+          text = "0x${base00}";
+          cursor = "0x${base05}";
+        };
+        normal = {
+          black = "0x${base00}";
+          red = "0x${base08}";
+          green = "0x${base0B}";
+          yellow = "0x${base0A}";
+          blue = "0x${base0D}";
+          magenta = "0x${base0E}";
+          cyan = "0x${base0C}";
+          white = "0x${base05}";
+        };
+        bright = {
+          black = "0x${base03}";
+          red = "0x${base08}";
+          green = "0x${base0B}";
+          yellow = "0x${base0A}";
+          blue = "0x${base0D}";
+          magenta = "0x${base0E}";
+          cyan = "0x${base0C}";
+          white = "0x${base07}";
+        };
+        indexed_colors = [
+          { index = 16; color = "0x${base09}"; }
+          { index = 17; color = "0x${base0F}"; }
+          { index = 18; color = "0x${base01}"; }
+          { index = 19; color = "0x${base02}"; }
+          { index = 20; color = "0x${base04}"; }
+          { index = 21; color = "0x${base06}"; }
+        ];
+      };
+    };
   };
 }
--- adapters/dunst.nix
+++ adapters/dunst.nix
@@ -6,25 +6,23 @@
   };
 
   config = lib.mkIf config.nixColorsAdapters.dunst.enable {
-    services.dunst.settings = with config.colorScheme.colors; ''
-      [global]
-      frame_color = "#${base05}"
-      separator_color = "#${base05}"
-
-      [base16_low]
-          msg_urgency = "low"
-          background = "#${base01}"
-          foreground = "#${base03}"
-
-      [base16_normal]
-          msg_urgency = "normal"
-          background = "#${base02}"
-          foreground = "#${base05}"
-
-      [base16_critical]
-          msg_urgency = "critical"
-          background = "#${base08}"
-          foreground = "#${base06}"
-    '';
+    services.dunst.settings = with config.colorScheme.colors; {
+      global = {
+        frame_color = "#${base05}";
+        separator_color = "#${base05}";
+      };
+      urgency_low = {
+        background = "#${base01}";
+        foreground = "#${base03}";
+      };
+      urgency_normal = {
+        background = "#${base02}";
+        foreground = "#${base05}";
+      };
+      urgency_critical = {
+        background = "#${base08}";
+        foreground = "#${base06}";
+      };
+    };
   };
 }
